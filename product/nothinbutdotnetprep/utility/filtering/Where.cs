8719e877205b5499b62c5c63267a17b2790a07e8
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-14 17:24:05 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/Where.cs
+++ product/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+//todo : Get Rid Of This
+using nothinbutdotnetprep.collections;
 
 namespace nothinbutdotnetprep.utility.filtering
 {
     public static class Where<ItemToFilter>
     {
-        public static object has_a<ReturnType>(PropertyAccessor<ItemToFilter, ReturnType> property_accessor)
+        public static Factory<ItemToFilter,ReturnType> has_a<ReturnType>(PropertyAccessor<ItemToFilter, ReturnType> property_accessor)
         {
-            throw new NotImplementedException();
+            return new Factory<ItemToFilter, ReturnType>(property_accessor);
         }
     }
+
+    public class Factory<ItemToMatch, PropertyType>{
+        private readonly PropertyAccessor<ItemToMatch, PropertyType> _propertyAccessor;
+
+        public Factory(PropertyAccessor<ItemToMatch, PropertyType> propertyAccessor)
+        {
+            _propertyAccessor = propertyAccessor;
+        }
+
+        public Criteria<ItemToMatch> equal_to(ProductionStudio value)
+        {
+            MatchingCondition<ItemToMatch> condition =
+                (movie) => _propertyAccessor.Invoke(movie).Equals(value);
+            return new ConditionalCriteria<ItemToMatch>(condition);
+        }
+    }
+
 }
