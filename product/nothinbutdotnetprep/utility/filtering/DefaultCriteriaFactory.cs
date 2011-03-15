be5d3bbffd927defe76e4ba922ef724cad4e6f4c
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-15 12:37:39 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/DefaultCriteriaFactory.cs
+++ product/nothinbutdotnetprep/utility/filtering/DefaultCriteriaFactory.cs
@@ -37,29 +37,4 @@ namespace nothinbutdotnetprep.utility.filtering
             get { return new NotCriteriaFactory<ItemToFilter, ReturnType>(this); }
         }
     }
-
-    public class NotCriteriaFactory<ItemToFilter,ReturnType>:CriteriaFactory<ItemToFilter,ReturnType>
-    {
-        private readonly DefaultCriteriaFactory<ItemToFilter, ReturnType> _defaultCriteriaFactory;
-
-        public NotCriteriaFactory(DefaultCriteriaFactory<ItemToFilter, ReturnType> defaultCriteriaFactory)
-        {
-            _defaultCriteriaFactory = defaultCriteriaFactory;
-        }
-
-        public Criteria<ItemToFilter> create_from(Criteria<ReturnType> property_criteria)
-        {
-            return _defaultCriteriaFactory.create_from(property_criteria).not();
-        }
-
-        public Criteria<ItemToFilter> equal_to(ReturnType value_to_equal)
-        {
-            return _defaultCriteriaFactory.equal_to(value_to_equal).not();
-        }
-
-        public Criteria<ItemToFilter> equal_to_any(params ReturnType[] values)
-        {
-            return _defaultCriteriaFactory.equal_to_any(values).not();
-        }
-    }
 }
---
+++ product/nothinbutdotnetprep/utility/filtering/NotCriteriaFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace nothinbutdotnetprep.utility.filtering
+{
+    public class NotCriteriaFactory<ItemToFilter,ReturnType>:CriteriaFactory<ItemToFilter,ReturnType>
+    {
+        private readonly DefaultCriteriaFactory<ItemToFilter, ReturnType> _defaultCriteriaFactory;
+
+        public NotCriteriaFactory(DefaultCriteriaFactory<ItemToFilter, ReturnType> defaultCriteriaFactory)
+        {
+            _defaultCriteriaFactory = defaultCriteriaFactory;
+        }
+
+        public Criteria<ItemToFilter> create_from(Criteria<ReturnType> property_criteria)
+        {
+            return _defaultCriteriaFactory.create_from(property_criteria).not();
+        }
+
+        public Criteria<ItemToFilter> equal_to(ReturnType value_to_equal)
+        {
+            return _defaultCriteriaFactory.equal_to(value_to_equal).not();
+        }
+
+        public Criteria<ItemToFilter> equal_to_any(params ReturnType[] values)
+        {
+            return _defaultCriteriaFactory.equal_to_any(values).not();
+        }
+    }
+}
